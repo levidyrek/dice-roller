eb5da8e5620c372645e64d8b69978a479496e8b4
Levi Payne <levi.dyrek@gmail.com>
2019-03-26 07:34:49 -0400

    Create some unit tests for main.rs.

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,41 @@ fn main() {
         process::exit(1);
     }
 }
+
+mod tests {
+    extern crate assert_cmd;
+
+    use std::process::Command;
+    use assert_cmd::prelude::*;
+
+    #[test]
+    fn config_valid_args() {
+        let mut cmd = Command::cargo_bin("dice_roller").unwrap();
+        cmd
+            .arg("2d8")
+            .assert()
+            .success();
+    }
+
+    #[test]
+    fn config_no_args() {
+        let mut cmd = Command::cargo_bin("dice_roller").unwrap();
+        cmd
+            .assert()
+            .failure()
+            .stderr("Problem parsing arguments: Didn't get a dice roll\n");
+    }
+
+    #[test]
+    fn config_bad_args() {
+        let mut cmd = Command::cargo_bin("dice_roller").unwrap();
+        cmd
+            .arg("test")
+            .assert()
+            .failure()
+            .stderr("\
+                Problem parsing arguments: Dice roll must be of the \
+                format: <num_of_rolls>d<sides_of_die>\n"
+            );
+    }
+}
